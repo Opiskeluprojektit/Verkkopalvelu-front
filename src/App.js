1c492be5c0e51fc83aa748ceb2e0ab0da4c4d8ac
jessepesse <58948883+jessepesse@users.noreply.github.com>
2021-11-08 17:03:45 +0200

    class muuttaminen classname

--- src/App.js
+++ src/App.js
@@ -35,85 +35,74 @@ function App() {
 </nav>
 <div className="container">
   <div className="row">
-    <div className="col">
-      <h3>Suositut</h3>
-    </div>
-    <div className="col">
-      <h3>Tarjoukset</h3>
-    </div>
-  </div>
-</div>
-<div className="container">
-  <div className="row">
 <div className="card col m-5">
-<div class="card">
-    <div class="card-body top-card">
+<div className="card">
+    <div className="card-body top-card">
       <h5>Suositut</h5>
       <a>Tutustu</a>
     </div>
   </div>
   <img src="img/cat.png" class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Kissa</h5>
-    <p class="card-text">Esimerkkitekstiä kissapiirrustuksesta.</p>
+  <div className="card-body">
+    <h5 className="card-title">Kissa</h5>
+    <p className="card-text">Esimerkkitekstiä kissapiirrustuksesta.</p>
     <a href=" " className="tuotesivulle">Tuotesivulle</a><br></br>
-    <i class="bi bi-cart-fill mx-2"></i>
+    <i className="bi bi-cart-fill mx-2"></i>
   </div>
 </div>
 <div className="card col m-5">
-<div class="card">
+<div className="card">
     <div class="card-body top-card">
       <h5>Maalaukset</h5>
       <a>Tutustu</a>
     </div>
   </div>
   <img src="img/trees.png" class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Puut</h5>
-    <p class="card-text">Maalauksesta esimerkkiasiaa.</p>
+  <div className="card-body">
+    <h5 className="card-title">Puut</h5>
+    <p className="card-text">Maalauksesta esimerkkiasiaa.</p>
     <a href=" " className="tuotesivulle">Tuotesivulle</a><br></br>
-    <i class="bi bi-cart-fill mx-2"></i>
+    <i className="bi bi-cart-fill mx-2"></i>
   </div>
 </div>
 <div className="card col m-5">
-<div class="card">
-    <div class="card-body top-card">
+<div className="card">
+    <div className="card-body top-card">
       <h5>Valokuvat</h5>
       <a>Tutustu</a>
     </div>
   </div>
   <img src="img/elephant-dark.png" class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Elefantti</h5>
-    <p class="card-text">Norsu-valokuvasta esimerkiksi asiaa.</p>
+  <div className="card-body">
+    <h5 className="card-title">Elefantti</h5>
+    <p className="card-text">Norsu-valokuvasta esimerkiksi asiaa.</p>
     <a href=" " className="tuotesivulle">Tuotesivulle</a><br></br>
-    <i class="bi bi-cart-fill mx-2"></i>
+    <i className="bi bi-cart-fill mx-2"></i>
   </div>
   </div>
 </div>
 </div>
 {/*className=" bg-light" footeriin->sama väri kuin navissa? 
 https://www.bootdey.com/snippets/view/white-footer#html footerin koodit löytyy täältä*/}
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-<footer class="footer_area section_padding_130_0">
-      <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-6 col-lg-4">
-            <div class="single-footer-widget section_padding_0_130">
-              <div class="footer-logo mb-3"></div>
+<footer className="footer_area section_padding_130_0">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-6 col-lg-4">
+            <div className="single-footer-widget section_padding_0_130">
+              <div className="footer-logo mb-3"></div>
               <p>Visual is a .....
                 Tähän lisätietoa kaupasta( Yhteystiedot jne?)
               </p>
-              <div class="copywrite-text mb-5">
-                <p class="mb-0">This footer is made with <i class="lni-heart mr-1"></i> by<a class="ml-1" href="https://wrapbootstrap.com/user/DesigningWorld"> Designing World</a></p>
+              <div className="copywrite-text mb-5">
+                <p className="mb-0">This footer is made with <i class="lni-heart mr-1"></i> by<a class="ml-1" href="https://wrapbootstrap.com/user/DesigningWorld"> Designing World</a></p>
               </div>
-              <div class="footer_social_area"><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook"><i class="fa fa-facebook"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Pinterest"><i class="fa fa-pinterest"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Skype"><i class="fa fa-skype"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter"><i class="fa fa-twitter"></i></a></div>
+              <div className="footer_social_area"><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook"><i class="fa fa-facebook"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Pinterest"><i class="fa fa-pinterest"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Skype"><i class="fa fa-skype"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter"><i class="fa fa-twitter"></i></a></div>
             </div>
           </div>
-          <div class="col-12 col-sm-6 col-lg">
-            <div class="single-footer-widget section_padding_0_130">
-              <h5 class="widget-title">About</h5>
-              <div class="footer_menu">
+          <div className="col-12 col-sm-6 col-lg">
+            <div className="single-footer-widget section_padding_0_130">
+              <h5 className="widget-title">About</h5>
+              <div className="footer_menu">
                 <ul>
                   <li><a href="#">About Us</a></li>
                   <li><a href="#">Corporate Sale</a></li>
@@ -123,10 +112,10 @@ https://www.bootdey.com/snippets/view/white-footer#html footerin koodit löytyy 
               </div>
             </div>
           </div>
-          <div class="col-12 col-sm-6 col-lg">
-            <div class="single-footer-widget section_padding_0_130">
-              <h5 class="widget-title">Support</h5>
-              <div class="footer_menu">
+          <div className="col-12 col-sm-6 col-lg">
+            <div className="single-footer-widget section_padding_0_130">
+              <h5 className="widget-title">Support</h5>
+              <div className="footer_menu">
                 <ul>
                   <li><a href="#">Help</a></li>
                   <li><a href="#">Support</a></li>
@@ -137,10 +126,10 @@ https://www.bootdey.com/snippets/view/white-footer#html footerin koodit löytyy 
               </div>
             </div>
           </div>
-          <div class="col-12 col-sm-6 col-lg">
-            <div class="single-footer-widget section_padding_0_130">
-              <h5 class="widget-title">Contact</h5>
-              <div class="footer_menu">
+          <div className="col-12 col-sm-6 col-lg">
+            <div className="single-footer-widget section_padding_0_130">
+              <h5 className="widget-title">Contact</h5>
+              <div className="footer_menu">
                 <ul>
                   <li><a href="#">Call Centre</a></li>
                   <li><a href="#">Email Us</a></li>
