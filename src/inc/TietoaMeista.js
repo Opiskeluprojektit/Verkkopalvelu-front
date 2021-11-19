6c10e819c6360d91788bbd3ff7052c7766dbcefe
lauravikstrom <81621759+lauravikstrom@users.noreply.github.com>
2021-11-19 18:54:40 +0200

    Tietoa meistä -sivun päivitys (sisältö ja css)

--- src/inc/TietoaMeista.js
+++ src/inc/TietoaMeista.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 export default function TietoaMeista() {
     return (
-        <div>
+        <div className="tietoameista">
             <h3>Tietoa meistä</h3>
-            <p>Visual on vuonna 2021 perustettu moderni taidekauppa. Idean takana on viisi 
-                innokasta opiskelijaa, jotka halusivat perustaa oman yrityksen. Visual 
-                on suunnattu ihmisille, jotka haluavat luoda sekä hankkia itselleen 
-                uniikkia taidetta ja samalla tukea suomalaisia taiteentekijöitä.</p>
+                <p>Visual on vuonna 2021 perustettu moderni taidekauppa. Visual 
+                    on suunnattu heille, jotka haluavat luoda sekä hankkia itselleen 
+                    uniikkia taidetta ja samalla tukea suomalaisia taiteentekijöitä.</p>
+
+
+                <p>Idea Visualiin syntyi ammattikorkeakoulukurssin luennolla. Idean 
+                    takana on viisi innokasta ja innovatiivista opiskelijaa, jotka halusivat 
+                    perustaa oman yrityksen.</p>
+
+                <p>Modernin taiteen tekijät myyvät usein taidettaan omilla sosiaalisen
+                    median tileillään. Osa heistä on myös perustanut oman sivustonsa
+                    tai tehnyt tilin erillisille myyntisivuille. Useiden taiteilijoiden 
+                    taidetta yhteen tuovia taidekauppoja ei löytynyt, minkä vuoksi 
+                    Visual perustettiin. </p>
         </div>
     )
 }
