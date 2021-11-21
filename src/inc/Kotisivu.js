39e88a2bc3bfcd7994311978d59418aa3e2e13bf
lauravikstrom <81621759+lauravikstrom@users.noreply.github.com>
2021-11-21 20:12:52 +0200

    Kotisivun päivitys ja Kotisivu.css-tiedoston lisäys

--- src/inc/Kotisivu.js
+++ src/inc/Kotisivu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Kotisivu.css';
 
 //Kotisivu
 export default function Kotisivu(addToCart) {
@@ -13,7 +14,7 @@ export default function Kotisivu(addToCart) {
             <a>Tutustu</a>
             </div>
         </div>
-        <img src="http://localhost/verkkopalveluback/img/Kissa.png" className="card-img-top" alt="..."/>
+        <img src="http://localhost/verkkopalveluback/img/Piirustus/Kissa.png" className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">Kissa</h5>
             <p className="card-text">Esimerkkitekstiä kissapiirrustuksesta.</p>
@@ -31,7 +32,7 @@ export default function Kotisivu(addToCart) {
             <a>Tutustu</a>
             </div>
         </div>
-        <img src="http://localhost/verkkopalveluback/img/Puut.png" className="card-img-top" alt="..."/>
+        <img src="http://localhost/verkkopalveluback/img/Illustrointi/Puut.png" className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">Puut</h5>
             <p className="card-text">Maalauksesta esimerkkiasiaa.</p>
@@ -48,7 +49,7 @@ export default function Kotisivu(addToCart) {
             <a>Tutustu</a>
             </div>
         </div>
-        <img src="http://localhost/verkkopalveluback/img/Elefantti2.png" className="card-img-top" alt="..."/>
+        <img src="http://localhost/verkkopalveluback/img/Valokuva/Elefantti2.png" className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">Elefantti</h5>
             <p className="card-text">Norsu-valokuvasta esimerkiksi asiaa.</p>
@@ -59,7 +60,53 @@ export default function Kotisivu(addToCart) {
         </div>
         </div>
         </div>
-        </div> 
+
+        <div className="row maisemajulisteet">
+            <div className="card">
+                <div className="card-body top-card-maisemajulisteet">
+                <h5>Maisemajulisteet</h5>
+                <a>Tutustu</a>
+                </div>
+            </div>
+            <img src="http://localhost/verkkopalveluback/img/Valokuva/Valokuva13.jpg" className="card-img-top" alt="..."/>
+        </div>
+
+        <div className="row inspiroidu">
+            <h5>INSPIROIDU</h5>
+            <p>Visual tuo sinulle modernin taiteen monissa eri muodoissa. Kauneus on katsojan silmissä, ja Visualin 
+                eri tyyleistä löydät varmasti omasi. Tauluilla voit luoda oman tyylisesi ilmapiirin, jossa silmä ja
+                sielu lepäävät. Korista ympäristösi piirustuksin, illustroinnein, valokuvin tai maalauksin. Löydät 
+                teoksista eri kategorioida, kuten abstraktia taikka luontoon tai eläimiin liittyviä teoksia. Olet 
+                värien tai mustavalkoisten teosten ystävä, Visualilta löydät molempia. Inspiroidu Visualin monista 
+                eri tyyleistä. 
+            </p>
+        </div>
+
+        <div className="row tarvikkeet">
+            <div className="card">
+                <div className="card-body top-card-tarvikkeet">
+                <h5>Tarvikkeet</h5>
+                <a>Tutustu</a>
+                </div>
+            </div>
+            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active" data-bs-interval="10000">
+                <img src="http://localhost/verkkopalveluback/img/Tarvikkeet/Kynät/Kynä2.png" class="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                <img src="http://localhost/verkkopalveluback/img/Tarvikkeet/Pensselit/Pensseli3.png" class="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item" data-bs-interval="2000">
+                <img src="http://localhost/verkkopalveluback/img/Tarvikkeet/Maalit/Maali1.png" class="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                <img src="http://localhost/verkkopalveluback/img/Tarvikkeet/Tarvikkeet3.jpg" class="d-block w-100" alt="..."/>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>        
         </div>
     )
 }
