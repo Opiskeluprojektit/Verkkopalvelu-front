abec68e8f3ccd0fcebde18ce2b3e8d03e0e0cfc2
lauravikstrom <81621759+lauravikstrom@users.noreply.github.com>
2021-11-30 16:02:36 +0200

    Tilaus.js-sivun tekoa. Koodissa virhe? Ei kysymysmerkkiä cartin jälkeen

--- src/inc/Tilaus.js
+++ src/inc/Tilaus.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React,{useEffect} from 'react';
 
-export default function Tilaus() {
+export default function Tilaus({cart}) {
+
+/*     useEffect(() => {
+
+    }, []) */
+
     return (
         <div>
-            <div>
-                Order
-            </div>
-            
+            <h3>Ostoskori</h3>
+            <table>
+            { cart?.map((product) => {
+                return (
+                    <tr>
+                        <td>{product.name}</td>   
+                        <td>{product.price}</td>                        
+                    </tr>
+                );
+            })}
+            </table>
         </div>
     )
 }
