19b99f1203f19577f2202ca450e71f2144e473b7
t8kohe02 <t8kohe02@students.oamk.fi>
2021-12-09 18:34:57 +0200

    admin sivun päivitys

--- src/inc/Admin.js
+++ src/inc/Admin.js
@@ -3,6 +3,10 @@ import React,{useState} from 'react';
 export default function Admin({url}) {
     const [newcategory, setNewcategory] = useState('');
     const [finished, setFinished] = useState(false);
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [image, setImage] = useState('');
+    const [category_id, setCategory_id] = useState('');
 
 
     function addCategory(e) {
@@ -21,14 +25,26 @@ export default function Admin({url}) {
         .then (res => {
             return res.json();
         })
-        // .then (
-        //     (res) => {
-        //         // empty();  
-        //         setFinished(true);
-        //     }, (error) => {
-        //         alert(error)
-        //     }
-        // )
+    }
+    function addProduct(e) {
+        e.preventDefault();
+        fetch(url + 'yllapito/addproduct.php',{
+            method: 'POST',
+            header: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name,
+                price: price,
+                image: image,
+                category_id: category_id,
+
+            })
+        })
+        .then (res => {
+            return res.json();
+        })
     }
     return (
         <div>
@@ -43,7 +59,28 @@ export default function Admin({url}) {
                         <button className="btn btn-primary">Lisää</button>
                     </div>
                 </form>
-            
+            <h3 className="Tilaustiedot">Tuotteen lisäys: </h3>
+                <form onSubmit={addProduct}>
+                        <div className="form-group">
+                            <label>Tuotteen nimi:</label>
+                            <input className="form-control" onChange={e => setName(e.target.value)}/>
+                        </div>
+                        <div className="form-group">
+                            <label>Tuotteen hinta:</label>
+                            <input className="form-control" onChange={e => setPrice(e.target.value)}/>
+                        </div>
+                        <div className="form-group">
+                            <label>Tuotteen kuvan nimi kokonaisuudessaan:</label>
+                            <input className="form-control" onChange={e => setImage(e.target.value)}/>
+                        </div>
+                        <div className="form-group">
+                            <label>Tuotteen kategorianumero:</label>
+                            <input className="form-control" onChange={e => setCategory_id(e.target.value)}/>
+                        </div>
+                        <div className="buttons">
+                            <button className="btn btn-primary">Lisää tuote</button>
+                        </div>
+                    </form>
         </div>
     )
 
