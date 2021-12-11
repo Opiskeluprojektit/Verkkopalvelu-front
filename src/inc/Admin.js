54f260a7e505011350455af845b47d5637d4800a
jessepesse <58948883+jessepesse@users.noreply.github.com>
2021-12-11 18:11:18 +0200

    Kuvan nimen lisääminen tietokantaan ja kuvan siirtäminen kansioon

--- src/inc/Admin.js
+++ src/inc/Admin.js
@@ -9,13 +9,31 @@ export default function Admin({url}) {
     const [category_id, setCategory_id] = useState('');
     const [files, setFiles] = useState();
 
-    console.log(files)
 
-
+ 
     const onImageChange = (event) => {
         setFiles(event.target.files[0]);
         setImage(event.target.files[0].name)
-    }
+    } 
+
+
+    const uploadImage = () => {
+        const formData = new FormData();
+        formData.append('File', files);
+
+        console.log(files)
+
+        fetch(url + 'yllapito/addphoto.php',{
+            method: 'POST',
+            body: formData,
+        })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log('Success:', result);
+        }).catch((error) => {
+            console.log('Error:', error);
+        })
+        };
 
     function addCategory(e) {
         e.preventDefault();
@@ -54,16 +72,6 @@ export default function Admin({url}) {
         })
     }
 
-  /*   const addPhoto = (event) => {
-        event.preventDefault();
-        const formData = new FormData();
-        formData.append('File', files)
-        fetch(url + 'yllapito/addphoto.php',{
-            method: 'POST',
-            body: formData,
-            })
-    } */
-
     return (
         <div>
             <h3 className="Tilaustiedot">Kategorian lisäys</h3>
@@ -97,10 +105,10 @@ export default function Admin({url}) {
                         </div>
                         <div class="mb-3">
                         <label for="formFile" class="form-label">Tuotteen kuvan lisäys:</label>
-                        <input class="form-control" onChange={onImageChange} type="file" id="formFile" name="file[]" />
+                        <input class="form-control" onChange={onImageChange} type="file" id="formFile" />
                         </div>
                         <div className="buttons">
-                            <button className="btn btn-primary">Lisää tuote</button>
+                            <button onClick={uploadImage} className="btn btn-primary">Lisää tuote</button>
                         </div>
                     </form>
         </div>
