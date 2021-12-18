6d82885e40b201c2cb4ada260689830627f20b97
Anne <n0koan00@students.oamk.fi>
2021-12-18 18:38:31 +0200

    muotilu ja haku button

--- src/inc/Kotisivu.js
+++ src/inc/Kotisivu.js
@@ -7,105 +7,105 @@ import { Link } from 'react-router-dom';
 export default function Kotisivu(addToCart) {
     return (
         <div>
-        <div className="container">
-        <div className="row">
-        <div className="d-flex justify-content-center col-lg-3 col-m-7">
-            <Link to="/Suositut">
-            <div className="card card2 mobiili">
-                <div className="card-body top-card">
-                <h5>Suositut</h5>
+            <div className="container">
+                <div className="row">
+                    <div className="d-flex justify-content-center col-lg-3 col-m-7">
+                        <Link to="/Suositut">
+                        <div className="card card2 mobiili">
+                            <div className="card-body top-card">
+                            <h5>Suositut</h5>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="http://localhost/verkkopalveluback/img/Piirustus16.png" className="card-img-top" alt="..."/>
+                        </div> 
+                        </Link>
+                    </div>
+                    <div className="d-flex justify-content-center col-lg-4 col-m-7">
+                        <Link to={{
+                            pathname: "/Tuotteet",
+                            state: {
+                                id: "4",
+                                name: "Illustrointi"
+                            }}}>
+                        <div className="card card2">
+                            <div className="card-body top-card">
+                            <h5>Illustrointi</h5>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="http://localhost/verkkopalveluback/img/Illustrointi24.png" className="card-img-top" alt="..."/>
+                        </div> 
+                        </Link>
+                    </div>
+                    <div className="d-flex justify-content-center col-lg-4 col-m-7">
+                        <Link to={{
+                            pathname: "/Tuotteet",
+                            state: {
+                                id: "3",
+                                name: "Valokuvat"
+                            }}}>
+                        <div className="card card2">
+                            <div className="card-body top-card">
+                            <h5>Valokuvat</h5>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="http://localhost/verkkopalveluback/img/Valokuva29.png" className="card-img-top" alt="..."/>
+                        </div> 
+                        </Link>
+                        </div>
+                    </div>
+                    <div className="row maisemajulisteet d-flex justify-content-center">
+                        <Link to="/Maisemajulisteet">
+                        <div className="card card2">
+                            <div className="card-body top-card-maisemajulisteet">
+                            <h5>Maisemajulisteet</h5>
+                            <a className="mobiiliversio">Tutustu</a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="http://localhost/verkkopalveluback/img/Valokuva13.jpg" className="card-img-top" alt="..."/>
+                        </div>
+                        </Link>
+                    </div>
+                    <div className="row inspiroidu">
+                        <h5>INSPIROIDU</h5>
+                        <p>Visual tuo sinulle modernin taiteen monissa eri muodoissa. Kauneus on katsojan silmissä, ja Visualin 
+                            eri tyyleistä löydät varmasti omasi. Tauluilla voit luoda oman tyylisesi ilmapiirin, jossa silmä ja
+                            sielu lepäävät. Korista ympäristösi piirustuksin, illustroinnein, valokuvin tai maalauksin. Löydät 
+                            teoksista eri kategorioida, kuten abstraktia taikka luontoon tai eläimiin liittyviä teoksia. Olet 
+                            värien tai mustavalkoisten teosten ystävä, Visualilta löydät molempia. Inspiroidu Visualin monista 
+                            eri tyyleistä. 
+                        </p>
+                    </div>
+                    <div className="row tarvikkeet">
+                        <h5>TARVIKKEET</h5>
+                        <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                        <Link to={{
+                            pathname: "/Tuotteet",
+                            state: {
+                                id: "5",
+                                name: "Tarvikkeet"
+                            }}}>
+                            <div className="carousel-item active" data-bs-interval="4000">
+                            <img src="http://localhost/verkkopalveluback/img/Tarvikkeet_Kyna2.png" class="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="carousel-item" data-bs-interval="4000">
+                            <img src="http://localhost/verkkopalveluback/img/Tarvikkeet_Pensseli3.png" class="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="carousel-item" data-bs-interval="4000">
+                            <img src="http://localhost/verkkopalveluback/img/Tarvikkeet_Maali1.png" class="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="carousel-item" data-bs-interval="4000">
+                            <img src="http://localhost/verkkopalveluback/img/Tarvikkeet3.jpg" class="d-block w-100" alt="..."/>
+                            </div>
+                            </Link>
+                        </div> 
+                    </div>
                 </div>
-            </div>
-            <div>
-                <img src="http://localhost/verkkopalveluback/img/Piirustus16.png" className="card-img-top" alt="..."/>
             </div> 
-            </Link>
-        </div>
-        <div className="d-flex justify-content-center col-lg-4 col-m-7">
-            <Link to={{
-                pathname: "/Tuotteet",
-                state: {
-                    id: "4",
-                    name: "Illustrointi"
-                }}}>
-            <div className="card card2">
-                <div className="card-body top-card">
-                <h5>Illustrointi</h5>
-                </div>
-            </div>
-            <div>
-                <img src="http://localhost/verkkopalveluback/img/Illustrointi24.png" className="card-img-top" alt="..."/>
-            </div> 
-            </Link>
-        </div>
-        <div className="d-flex justify-content-center col-lg-4 col-m-7">
-            <Link to={{
-                pathname: "/Tuotteet",
-                state: {
-                    id: "3",
-                    name: "Valokuvat"
-                }}}>
-            <div className="card card2">
-                <div className="card-body top-card">
-                <h5>Valokuvat</h5>
-                </div>
-            </div>
-            <div>
-                <img src="http://localhost/verkkopalveluback/img/Valokuva29.png" className="card-img-top" alt="..."/>
-            </div> 
-            </Link>
-        </div>
-        </div>
-        <div className="row maisemajulisteet d-flex justify-content-center">
-            <Link to="/Maisemajulisteet">
-            <div className="card card2">
-                <div className="card-body top-card-maisemajulisteet">
-                <h5>Maisemajulisteet</h5>
-                <a className="mobiiliversio">Tutustu</a>
-                </div>
-            </div>
-            <div>
-                <img src="http://localhost/verkkopalveluback/img/Valokuva13.jpg" className="card-img-top" alt="..."/>
-            </div>
-            </Link>
-        </div>
-        <div className="row inspiroidu">
-            <h5>INSPIROIDU</h5>
-            <p>Visual tuo sinulle modernin taiteen monissa eri muodoissa. Kauneus on katsojan silmissä, ja Visualin 
-                eri tyyleistä löydät varmasti omasi. Tauluilla voit luoda oman tyylisesi ilmapiirin, jossa silmä ja
-                sielu lepäävät. Korista ympäristösi piirustuksin, illustroinnein, valokuvin tai maalauksin. Löydät 
-                teoksista eri kategorioida, kuten abstraktia taikka luontoon tai eläimiin liittyviä teoksia. Olet 
-                värien tai mustavalkoisten teosten ystävä, Visualilta löydät molempia. Inspiroidu Visualin monista 
-                eri tyyleistä. 
-            </p>
-        </div>
-        <div className="row tarvikkeet">
-            <h5>TARVIKKEET</h5>
-            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-            <Link to={{
-                pathname: "/Tuotteet",
-                state: {
-                    id: "5",
-                    name: "Tarvikkeet"
-                }}}>
-                <div className="carousel-item active" data-bs-interval="4000">
-                <img src="http://localhost/verkkopalveluback/img/Tarvikkeet_Kyna2.png" class="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item" data-bs-interval="4000">
-                <img src="http://localhost/verkkopalveluback/img/Tarvikkeet_Pensseli3.png" class="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item" data-bs-interval="4000">
-                <img src="http://localhost/verkkopalveluback/img/Tarvikkeet_Maali1.png" class="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item" data-bs-interval="4000">
-                <img src="http://localhost/verkkopalveluback/img/Tarvikkeet3.jpg" class="d-block w-100" alt="..."/>
-                </div>
-                </Link>
-            </div> 
-        </div>
-        </div>
-        </div>        
         </div>
     )
 }
--- src/inc/Navbar.js
+++ src/inc/Navbar.js
@@ -37,61 +37,60 @@ export default function Navbar({cart, setCategory, url, search}) {
     );
   };
 
-    {/* Navigointinapit etusivulle ja tuoteryhmiin */}
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Visual</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Etusivu</Link>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tuotteet</a>
-                  <ul className="dropdown-menu">
-                    {categories.map(category =>(
-                      <li key={category.id}>
-                        <Link className="dropdown-item"
-                        to={{
-                          pathname: '/Tuotteet',
-                          state: {
-                            id: category.id,
-                            name: category.name,
-                          }
-                        }}
-                        >
-                        {category.name}
-                        </Link>
-                      </li>
-                    ))}
-                    <li>
-                    <Link className="dropdown-item" to="/Suositut">Suositut</Link>
-                    </li>
-                  </ul>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/Yllapito">Ylläpito</Link>
-              </li>
-            </ul>
-            {/* Haku-kenttä */}
-            <div>
-            <form onSubmit={handleSubmit} className="d-flex" >
-              <input className="form-control me-2" type="search" placeholder="Haku" aria-label="Search" 
-              onChange={e => setName(e.target.value)}/>
-            <button className="btn btn-outline-success" onClick={handleSubmit}><Link to="/Tuotteet">Haku</Link></button>
-            </form>
-            </div>
-
-            {/* Ostoskori */}
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item"></li>
-              <Ostoskori cart={cart}/>
-            </ul>
-          </div>
+  {/* Navigointinapit etusivulle ja tuoteryhmiin */}
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to="/">Visual</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/">Etusivu</Link>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tuotteet</a>
+              <ul className="dropdown-menu">
+                {categories.map(category =>(
+                  <li key={category.id}>
+                    <Link className="dropdown-item"
+                    to={{
+                      pathname: '/Tuotteet',
+                      state: {
+                        id: category.id,
+                        name: category.name,
+                      }
+                    }}
+                    >
+                    {category.name}
+                    </Link>
+                  </li>
+                ))}
+                <li>
+                  <Link className="dropdown-item" to="/Suositut">Suositut</Link>
+                </li>
+              </ul>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" aria-current="page" to="/Yllapito">Ylläpito</Link>
+          </li>
+        </ul>
+        {/* Haku-kenttä */}
+        <div>
+          <form onSubmit={handleSubmit} className="d-flex" >
+            <input className="form-control me-2" type="search" placeholder="Haku" aria-label="Search" 
+            onChange={e => setName(e.target.value)}/>
+          <button className="btn btn-outline-secondary" onClick={handleSubmit}><Link to="/Tuotteet">Haku</Link></button>
+          </form>
+        </div>
+        {/* Ostoskori */}
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item"></li>
+          <Ostoskori cart={cart}/>
+        </ul>
+    </div>
     </div>
     </nav>   
   )
