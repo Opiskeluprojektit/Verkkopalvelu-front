72d5f552ba2cd6aca28803a6c82df873c6e66476
unknown <n0ruje00@students.oamk.fi>
2021-12-19 22:45:30 +0200

    tuote -sivun muotoilua

--- src/inc/Tuote.js
+++ src/inc/Tuote.js
@@ -9,23 +9,31 @@ export default function Tuote({ url,product,addToCart }) {
         <div className="col-6">
           {/* yksittäisen tuotteen näyttäminen */}
           <h5>{product?.name}</h5>
-          <div>
+           <div>
             <img src={url + "IMG/" + product?.image} alt="" />
             </div>
           {/* Tuotteen lisäys ostoskoriin-painike */}
-          <p>{product?.price} €</p>
+          <div>
+           <div className="d-flex">
+             <div className="hinta">
+               <p>{product?.price} €</p>
+             </div>
+             <div>
+               <button className="btn-light" type="button" onClick={e => addToCart(product)}>
+        Lisää     <i className="bi bi-cart-fill mx-2"></i>
+               </button>
+             </div>
+          </div>
+          </div>
         </div>
       </div>
-      <button className="btn-light" type="button" onClick={e => addToCart(product)}>
-        Lisää <i className="bi bi-cart-fill mx-2"></i>
-      </button>
       { /*Tuotekuvauksen näyttäminen*/}
       <div className="row justify-content-center">
        <div className="col-6">
             <div className="card-w-100">
               <div class="card-body">
-              <h2 class="card-title">Tuotekuvaus:</h2>
-              <p class="card-text">{product?.description}</p>
+               <h2 class="card-title">Tuotekuvaus:</h2>
+                 <p class="card-text">{product?.description}</p>
               </div>
             </div>
        </div>
