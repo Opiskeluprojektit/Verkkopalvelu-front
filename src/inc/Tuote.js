beab0fc8c08844a4fbfd8d6dfeb79995581ace1d
unknown <n0ruje00@students.oamk.fi>
2021-12-19 16:17:34 +0200

    tuotekuvauksen asettelua

--- src/inc/Tuote.js
+++ src/inc/Tuote.js
@@ -4,24 +4,32 @@ import React from "react";
 //Näyttää yhden tuotteen sivustolla
 export default function Tuote({ url,product,addToCart }) {
   return (
-    <div id="tuote" className="container justify-content-center">
+    <div id="tuote" className="container justify-content-center ">
       <div className="row justify-content-center">
         <div className="col-6">
-          {/* yksittäisen tuotteen näyttäminen */}
+          {/* yksittäisen tuotteen näyttäminen / AK */}
           <h5>{product?.name}</h5>
           <div>
             <img src={url + "IMG/" + product?.image} alt="" />
-           { /*Tuote kuvauksen näyttäminen*/}
-           <h2>TUOTEKUVAUS:</h2>
-            <p>{product?.description}</p>
             </div>
-          {/* Tuotteen lisäys ostoskoriin-painike */}
+          {/* Tuotteen lisäys ostoskoriin-painike /AK */}
           <p>{product?.price} €</p>
         </div>
       </div>
       <button className="btn-light" type="button" onClick={e => addToCart(product)}>
         Lisää <i className="bi bi-cart-fill mx-2"></i>
       </button>
+      { /*Tuotekuvauksen näyttäminen*/}
+      <div className="row justify-content-center">
+       <div className="col-6">
+            <div className="card-w-100">
+              <div class="card-body">
+              <h2 class="card-title">Tuotekuvaus:</h2>
+              <p class="card-text">{product?.description}</p>
+              </div>
+            </div>
+       </div>
+      </div>
     </div>
   );
 }
